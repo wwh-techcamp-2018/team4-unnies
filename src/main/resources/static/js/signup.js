46368acfcb80be9325a520a8302b8e6f87ee06d1
sheep1500 <woowatechcamp_17@woowatechcamp17.local>
2018-08-15 19:25:23 +0900

    [feature] F/E, signup layout 구현 및 daum 주소 api 적용 (#19, #20) -  signup.html : 회원가입 폼 생성 -  signup.js : daum 주소 api script 적용,  회원가입 데이터 서버 전송

---
+++ src/main/resources/static/js/signup.js
@@ -0,0 +1,111 @@
+function $(selector){
+    return document.querySelector(selector);
+}
+
+$("#button").addEventListener("click",signupHandler);
+
+function signupHandler(evt){
+    evt.preventDefault();
+
+    const email = `${$("#email").value}`;
+    const name = `${$("#name").value}`;
+    const password = `${$("#password").value}`;
+    const confirmPassword = `${$("#confirm").value}`;
+    const phoneNumber= `${$("#phone").value}`;
+    const address=`${$("#address").value} ${$("#address_detail").value}`;
+
+    fetch('/api/users',{
+        method:'post',
+        headers:{'content-type':'application/json'},
+        credentials:"same-origin",
+        body: JSON.stringify({
+            email,
+            name,
+            password,
+            confirmPassword,
+            phoneNumber,
+            address
+        })
+    })
+    .then(response=>{
+        console.log(response);
+        if(!response.ok){
+            return location.reload();
+        }
+        alert("회원가입을 축하드립니다.");
+    })
+    .catch(error=>{
+        console.log(error);
+        location.reload();
+    });
+}
+
+
+// 우편번호 찾기 화면을 넣을 element
+var element_layer = document.getElementById('layer');
+
+function closeDaumPostcode() {
+    // iframe을 넣은 element를 안보이게 한다.
+    element_layer.style.display = 'none';
+}
+
+function sample2_execDaumPostcode() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+            // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+            var fullAddr = data.address; // 최종 주소 변수
+            var extraAddr = ''; // 조합형 주소 변수
+
+            // 기본 주소가 도로명 타입일때 조합한다.
+            if(data.addressType === 'R'){
+                //법정동명이 있을 경우 추가한다.
+                if(data.bname !== ''){
+                    extraAddr += data.bname;
+                }
+                // 건물명이 있을 경우 추가한다.
+                if(data.buildingName !== ''){
+                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                // 조합형주소의 유무에 따라 양쪽에 괄호를 추가하여 최종 주소를 만든다.
+                fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
+            }
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            document.getElementById('postcode').value = data.zonecode; //5자리 새우편번호 사용
+            document.getElementById('address').value = fullAddr;
+
+            // iframe을 넣은 element를 안보이게 한다.
+            // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
+            element_layer.style.display = 'none';
+        },
+        width : '100%',
+        height : '100%',
+        maxSuggestItems : 5
+    }).embed(element_layer);
+
+    // iframe을 넣은 element를 보이게 한다.
+    element_layer.style.display = 'block';
+
+    // iframe을 넣은 element의 위치를 화면의 가운데로 이동시킨다.
+    initLayerPosition();
+}
+
+// 브라우저의 크기 변경에 따라 레이어를 가운데로 이동시키고자 하실때에는
+// resize이벤트나, orientationchange이벤트를 이용하여 값이 변경될때마다 아래 함수를 실행 시켜 주시거나,
+// 직접 element_layer의 top,left값을 수정해 주시면 됩니다.
+function initLayerPosition(){
+    var width = 400; //우편번호서비스가 들어갈 element의 width
+    var height = 500; //우편번호서비스가 들어갈 element의 height
+    var borderWidth = 1; //샘플에서 사용하는 border의 두께
+
+    // 위에서 선언한 값들을 실제 element에 넣는다.
+    element_layer.style.width = width + 'px';
+    element_layer.style.height = height + 'px';
+    element_layer.style.border = borderWidth + 'px solid';
+    // 실행되는 순간의 화면 너비와 높이 값을 가져와서 중앙에 뜰 수 있도록 위치를 계산한다.
+    element_layer.style.left = (((window.innerWidth || document.documentElement.clientWidth) - width)/2 - borderWidth) + 'px';
+    element_layer.style.top = (((window.innerHeight || document.documentElement.clientHeight) - height)/2 - borderWidth) + 'px';
+}
