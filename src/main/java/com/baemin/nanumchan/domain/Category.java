7693e9dd06028e5c37fe079f3f3939a5a68b38b6
JiwooHong <tech_jiwoo@woowahan.com>
2018-08-16 13:00:36 +0900

    add : category entity (#11)

---
+++ src/main/java/com/baemin/nanumchan/domain/Category.java
@@ -0,0 +1,14 @@
+package com.baemin.nanumchan.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
