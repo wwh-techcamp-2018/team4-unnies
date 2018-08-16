0cdb5613e31a6b5d61141de5c05a08d7cc8c00c9
JiwooHong <tech_jiwoo@woowahan.com>
2018-08-16 19:16:47 +0900

    refactor : ApiProductAcceptanceTest 중복 코드 제거

--- src/test/java/com/baemin/nanumchan/web/ApiProductAcceptanceTest.java
+++ src/test/java/com/baemin/nanumchan/web/ApiProductAcceptanceTest.java
@@ -2,10 +2,10 @@ package com.baemin.nanumchan.web;
 
 import com.baemin.nanumchan.domain.Category;
 import com.baemin.nanumchan.domain.CategoryRepository;
-import com.baemin.nanumchan.domain.Product;
 import com.baemin.nanumchan.dto.ProductDto;
 import common.RestResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,26 +22,33 @@ public class ApiProductAcceptanceTest extends AcceptanceTest {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @Test
-    public void upload() {
+    private Category newCategory;
+    private ProductDto productDto;
+
+
+    @Before
+    public void setUp() throws Exception {
         Category category = Category.builder()
                 .name("Good")
                 .build();
 
-        Category newCategory = categoryRepository.save(category);
+        newCategory = categoryRepository.save(category);
 
-        ProductDto productDto = ProductDto.builder()
+        productDto = ProductDto.builder()
                 .categoryId(newCategory.getId())
+                .name("이름")
+                .price(1000L)
+                .title("제목")
                 .description("디스크립션")
+                .maxParticipant(3)
+                .isBowlNeeded(false)
                 .expireDateTime(LocalDateTime.now().toLocalDate())
                 .shareDateTime(LocalDateTime.now().toLocalDate())
-                .name("이름")
-                .title("제목")
-                .maxParticipant(3)
-                .price(1000L)
-                .isBowlNeeded(false)
                 .build();
+    }
 
+    @Test
+    public void upload() {
         ResponseEntity<Void> response = template.postForEntity("/api/products", productDto, Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         assertThat(response.getHeaders().getLocation().getPath()).isNotEmpty();
@@ -49,23 +56,7 @@ public class ApiProductAcceptanceTest extends AcceptanceTest {
 
     @Test
     public void upload_유효하지않은필드에러메시지_이름없음() {
-        Category category = Category.builder()
-                .name("Good")
-                .build();
-
-        Category newCategory = categoryRepository.save(category);
-
-        ProductDto productDto = ProductDto.builder()
-                .categoryId(newCategory.getId())
-                .description("디스크립션")
-                .expireDateTime(LocalDateTime.now().toLocalDate())
-                .shareDateTime(LocalDateTime.now().toLocalDate())
-                //.name()
-                .title("제목")
-                .maxParticipant(3)
-                .price(1000L)
-                .isBowlNeeded(false)
-                .build();
+        productDto.setName(null);
 
         ResponseEntity<RestResponse> response = template.postForEntity("/api/products", productDto, RestResponse.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
@@ -74,17 +65,7 @@ public class ApiProductAcceptanceTest extends AcceptanceTest {
 
     @Test
     public void upload_카테고리없음() {
-        ProductDto productDto = ProductDto.builder()
-                .categoryId(-1L)
-                .description("디스크립션")
-                .expireDateTime(LocalDateTime.now().toLocalDate())
-                .shareDateTime(LocalDateTime.now().toLocalDate())
-                .name("이름")
-                .title("제목")
-                .maxParticipant(3)
-                .price(1000L)
-                .isBowlNeeded(false)
-                .build();
+        productDto.setCategoryId(-1L);
 
         ResponseEntity<RestResponse> response = template.postForEntity("/api/products", productDto, RestResponse.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
