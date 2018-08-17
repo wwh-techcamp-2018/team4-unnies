5ea82fc1cda0236594f6a44d770dd10390e5ecb3
1500sheep <cjswotl6274@naver.com>
2018-08-17 21:44:20 +0900

    feat : login 레이아웃 수정 (#30)

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,41 +1,34 @@
-function $(selector){
-    return document.querySelector(selector);
-}
-
-$("#button").addEventListener("click",loginHandler);
+$('#button').addEventListener('click',loginHandler);
 
 const registerFlag = {
-    "email":false,
-    "password":false,
+    'email':false,
+    'password':false,
 };
 
 function loginHandler(evt){
     evt.preventDefault();
 
-    const email = `${$("#email").value}`;
-    const password = `${$("#password").value}`;
+    const email = $('#email').value;
+    const password = $('#password').value;
 
     fetch('/api/users/login',{
         method:'post',
         headers:{'content-type':'application/json'},
-        credentials:"same-origin",
+        credentials:'same-origin',
         body: JSON.stringify({
             email,
             password
         })
     })
     .then(response=>{
-        console.log(response);
-        if(response.status === 200){
-            alert("로그인을 하였습니다.");
-//            return location.reload();
-        }else{
-            alert("로그인에 실패하였습니다.");
+        if(response.status >= 400 && response.status <= 404){
             validateError(response);
+            return location.reload();
+        }else if(response.status === 200){
+            alert('로그인을 하였습니다.');
         }
     })
     .catch(error=>{
-        console.log(error);
         location.reload();
     });
 }
@@ -44,15 +37,15 @@ function validateError(response){
     response.json().then(({errors})=>{
         errors.forEach((error)=>{
             switch(error.field){
-                case "email" :
-                    $("#email").parentElement.nextElementSibling.style.visibility="visible";
-                    $("#email").parentElement.nextElementSibling.innerText=error.message;
-                    registerFlag["email"] = false;
+                case 'email' :
+                    $('#invalid-email').style.visibility='visible';
+                    $('#invalid-email').innerText=error.message;
+                    registerFlag['email'] = false;
                     break;
-                case "password" :
-                    $("#password").parentElement.nextElementSibling.style.visibility="visible";
-                    $("#password").parentElement.nextElementSibling.innerText=error.message;
-                    registerFlag["password"] = false;
+                case 'password' :
+                    $('#invalid-password').style.visibility='visible';
+                    $('#invalid-password').innerText=error.message;
+                    registerFlag['password'] = false;
                     break;
             }
         });
@@ -62,23 +55,23 @@ function validateError(response){
 
 function validateCheck(value){
     switch(value){
-        case "email":
+        case 'email':
             validateEmail();
             break;
-        case "password":
+        case 'password':
             validatePassword();
             break;
     }
 
     if(monitorRegisterButton()){
-        $("#button").disabled=false;
+        $('#button').disabled=false;
     }else{
-        $("#button").disabled=true;
+        $('#button').disabled=true;
     }
 }
 
 function monitorRegisterButton(){
-    for(let key in registerFlag){
+    for(const key in registerFlag){
             if(registerFlag[key] === false){
                 return false;
             }
@@ -89,27 +82,27 @@ function monitorRegisterButton(){
 function validateEmail(){
 
     const regex_email = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\w+\.)+\w+$/i;// 이메일이 적합한지 검사할 정규식
-    const email = `${$("#email").value}`;
+    const email = $('#email').value;
 
     if(!email.match(regex_email)){
-        $("#email").parentElement.nextElementSibling.style.visibility="visible";
-        registerFlag["email"] = false;
+        $('#invalid-email').style.visibility='visible';
+        registerFlag['email'] = false;
     }else{
-        $("#email").parentElement.nextElementSibling.style.visibility="hidden";
-        registerFlag["email"] = true;
+        $('#invalid-email').style.visibility='hidden';
+        registerFlag['email'] = true;
     }
 }
 
 function validatePassword(){
 
-    var regex_password = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/; // 아이디와 패스워드가 적합한지 검사할 정규식
-    const password = `${$("#password").value}`;
+    const regex_password = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/; // 아이디와 패스워드가 적합한지 검사할 정규식
+    const password = $('#password').value;
     if(!password.match(regex_password)){
-        $("#password").parentElement.nextElementSibling.style.visibility="visible";
-        registerFlag["password"] = false;
+        $('#invalid-password').style.visibility='visible';
+        registerFlag['password'] = false;
     }else{
-        $("#password").parentElement.nextElementSibling.style.visibility="hidden";
-        registerFlag["password"] = true;
+        $('#invalid-password').style.visibility='hidden';
+        registerFlag['password'] = true;
     }
 
 }
