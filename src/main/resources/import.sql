03ea14ee1014ea79bba165aa4d87c9acb512cea3
woowatechcamp_23 <tech_syl@woowahan.com>
2018-08-17 19:23:21 +0900

    feat : 카테고리 리스트 API 추가 (#45)

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,14 @@
 -- password origin : 123456a!
 insert into user(id, email, password, name, phone_number, address, about_me) values(1, 'unnies@naver.com', '$2a$10$VWzPAYMsYevYEppl.gXvgewcl4fVPpIuBVrtPENSg1I4bu5.Nfzxu', '강석윤', '010-1111-2222', '서울특별시 배민동 배민아파트',null);
+
+INSERT INTO Category(id, name) VALUES (1, '한식');
+INSERT INTO Category(id, name) VALUES (2, '분신');
+INSERT INTO Category(id, name) VALUES (3, '돈까스회일식');
+INSERT INTO Category(id, name) VALUES (4, '치킨');
+INSERT INTO Category(id, name) VALUES (5, '피자');
+INSERT INTO Category(id, name) VALUES (6, '중국집');
+INSERT INTO Category(id, name) VALUES (7, '족발보쌈');
+INSERT INTO Category(id, name) VALUES (8, '야식');
+INSERT INTO Category(id, name) VALUES (9, '찜탕');
+INSERT INTO Category(id, name) VALUES (10, '카페디져트');
+INSERT INTO Category(id, name) VALUES (11, '패스트푸드');
---
+++ src/test/java/com/baemin/nanumchan/web/ApiCategoryAcceptanceTest.java
@@ -0,0 +1,19 @@
+package com.baemin.nanumchan.web;
+
+import common.RestResponse;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import support.AcceptanceTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApiCategoryAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    public void list() {
+        ResponseEntity<RestResponse> response = template.getForEntity("/api/categories", RestResponse.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getData()).isNotNull();
+    }
+}
