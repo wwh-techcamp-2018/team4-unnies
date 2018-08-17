db99e845b4a88429440a65c73d66825d79f61151
1500sheep <cjswotl6274@naver.com>
2018-08-17 21:40:55 +0900

    feat : ApiUserController login 구현 (#27)

--- src/main/java/com/baemin/nanumchan/web/ApiUserController.java
+++ src/main/java/com/baemin/nanumchan/web/ApiUserController.java
@@ -1,9 +1,11 @@
-package com.baemin.nanumchan;
+package com.baemin.nanumchan.web;
 
 import com.baemin.nanumchan.domain.User;
+import com.baemin.nanumchan.dto.LoginDTO;
 import com.baemin.nanumchan.dto.SignUpDTO;
 import com.baemin.nanumchan.service.UserService;
 import com.baemin.nanumchan.utils.RestResponse;
+import com.baemin.nanumchan.utils.SessionUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.net.URI;
 
@@ -26,4 +29,12 @@ public class ApiUserController {
          User user = userService.save(signUpDTO);
          return ResponseEntity.created(URI.create("/users/" + user.getId())).build();
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<RestResponse> login(@Valid @RequestBody LoginDTO loginDTO, HttpSession session) {
+        User user = userService.login(loginDTO);
+        SessionUtils.setUserInSession(session, user);
+        return ResponseEntity.ok().build();
+    }
+
 }
