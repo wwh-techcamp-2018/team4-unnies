f5fa02dbf4995c2cf0e1cea4889cb113689e5aad
1500sheep <cjswotl6274@naver.com>
2018-08-17 21:42:26 +0900

    feat : loginDTO 생성 및 validation check (#27)

---
+++ src/main/java/com/baemin/nanumchan/dto/LoginDTO.java
@@ -0,0 +1,28 @@
+package com.baemin.nanumchan.dto;
+
+import com.baemin.nanumchan.domain.User;
+import com.baemin.nanumchan.utils.ValidateRegex;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.validation.constraints.Pattern;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDTO {
+
+    @Pattern(regexp = ValidateRegex.EMAIL)
+    private String email;
+
+    @Pattern(regexp = ValidateRegex.PASSWORD)
+    private String password;
+
+    public boolean matchPassword(PasswordEncoder passwordEncoder, User user) {
+        return passwordEncoder.matches(password, user.getPassword());
+    }
+}
