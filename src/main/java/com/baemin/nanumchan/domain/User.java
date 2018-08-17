86d8c86b825bb23201f408db9622109f4c3e0983
1500sheep <cjswotl6274@naver.com>
2018-08-17 16:14:51 +0900

    refactor : domain User @Column 추가

--- src/main/java/com/baemin/nanumchan/domain/User.java
+++ src/main/java/com/baemin/nanumchan/domain/User.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Builder
@@ -20,16 +17,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String phoneNumber;
 
+    @Column(nullable = false)
     private String address;
 
+    @Lob
     private String aboutMe;
 
 }
