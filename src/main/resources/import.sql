303e49dd8cbf4fe9d3c7c79912161a8ddbba1e1e
1500sheep <cjswotl6274@naver.com>
2018-08-17 21:43:26 +0900

    add : import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+-- password origin : 123456a!
+insert into user(id, email, password, name, phone_number, address, about_me) values(1, 'unnies@naver.com', '$2a$10$VWzPAYMsYevYEppl.gXvgewcl4fVPpIuBVrtPENSg1I4bu5.Nfzxu', '강석윤', '010-1111-2222', '서울특별시 배민동 배민아파트',null);
