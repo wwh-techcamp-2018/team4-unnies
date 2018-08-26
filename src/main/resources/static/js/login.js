60a541fcc9fb898e2e25c0d117be2810cfb4b3f9
1500sheep <cjswotl6274@naver.com>
2018-08-26 19:49:07 +0900

    refactor : F/E , login, signup 변경

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -8,8 +8,8 @@ const registerFlag = {
 function loginHandler(event){
     event.preventDefault();
 
-    const email = $('#email').value;
-    const password = $('#password').value;
+    const email = $('input[name=email]').value;
+    const password = $('input[name=password]').value;
 
     fetch('/api/users/login',{
         method:'post',
@@ -37,13 +37,13 @@ function validateError(response){
         errors.forEach((error)=>{
             switch(error.field){
                 case 'email' :
-                    $('#invalid-email').style.visibility='visible';
-                    $('#invalid-email').innerText=error.message;
+                    $('strong[name=invalid-email]').style.visibility='visible';
+                    $('strong[name=invalid-email]').innerText=error.message;
                     registerFlag['email'] = false;
                     break;
                 case 'password' :
-                    $('#invalid-password').style.visibility='visible';
-                    $('#invalid-password').innerText=error.message;
+                    $('strong[name=invalid-password]').style.visibility='visible';
+                    $('strong[name=invalid-password]').innerText=error.message;
                     registerFlag['password'] = false;
                     break;
             }
@@ -72,13 +72,13 @@ function monitorRegisterButton(){
 function validateEmail(){
 
     const regex_email = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\w+\.)+\w+$/i;// 이메일이 적합한지 검사할 정규식
-    const email = $('#email').value;
+    const email = $('input[name=email]').value;
 
     if(!email.match(regex_email)){
-        $('#invalid-email').style.visibility='visible';
+        $('strong[name=invalid-email]').style.visibility='visible';
         registerFlag['email'] = false;
     }else{
-        $('#invalid-email').style.visibility='hidden';
+        $('strong[name=invalid-email]').style.visibility='hidden';
         registerFlag['email'] = true;
         validateCheck();
     }
@@ -87,20 +87,18 @@ function validateEmail(){
 function validatePassword(){
 
     const regex_password = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/; // 아이디와 패스워드가 적합한지 검사할 정규식
-    const password = $('#password').value;
+    const password = $('input[name=password]').value;
     if(!password.match(regex_password)){
-        $('#invalid-password').style.visibility='visible';
+        $('strong[name=invalid-password]').style.visibility='visible';
         registerFlag['password'] = false;
     }else{
-        $('#invalid-password').style.visibility='hidden';
+        $('strong[name=invalid-password]').style.visibility='hidden';
         registerFlag['password'] = true;
         validateCheck();
     }
 
 }
-document.addEventListener('DOMContentLoaded', () => {
-    $('#button').addEventListener('click',loginHandler);
 
-    $('#email').onchange = validateEmail;
-    $('#password').onchange = validatePassword;
-});
+$('#button').addEventListener('click',loginHandler);
+$('input[name=email]').onchange = validateEmail;
+$('input[name=password]').onchange = validatePassword;
--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,13 +1,21 @@
 import { $ } from './lib/utils.js';
-import { closeDaumPostcode, execDaumPostcode } from './address.js';
+import DaumPostCode from './lib/DaumPostCode.js';
+
 const registerFlag = {
     'email':false,
     'name':false,
     'password':false,
     'confirmPassword':false,
-    'phone':false,
+    'phoneNumber':false,
     'addressDetail':false
 };
+
+const daumPostCode  = new DaumPostCode({
+      input: $('input[name=address]'),
+      searchButton: $('input[name=find-postcode]')
+});
+
+daumPostCode.load();
 
 function signupHandler(event){
     event.preventDefault();
@@ -17,14 +25,14 @@ function signupHandler(event){
         return;
     }
 
-    const email = $('#email').value;
-    const name = $('#name').value;
-    const password = $('#password').value;
-    const confirmPassword = $('#confirm').value;
-    const phoneNumber= $('#phone').value;
+    const email = $('input[name=email]').value;
+    const name = $('input[name=name]').value;
+    const password = $('input[name=password]').value;
+    const confirmPassword = $('input[name=confirm]').value;
+    const phoneNumber= $('input[name=phoneNumber]').value;
 
-    const address= $('#address').value;
-    const addressDetail = $('#address-detail').value;
+    const address= $('input[name=address]').value;
+    const addressDetail = $('input[name=address-detail]').value;
 
     fetch('/api/users',{
         method:'post',
@@ -53,47 +61,18 @@ function signupHandler(event){
 }
 
 function validateError(response){
-    response.json().then(({errors}) => {
+    response.json().then(({ errors }) => {
         errors.forEach((error) => {
-            switch(error.field){
-                case 'email' :
-                    $('#invalid-email').style.visibility='visible';
-                    $('#invalid-email').innerText=error.message;
-                    registerFlag['email'] = false;
-                    break;
-                case 'password' :
-                    $('#invalid-password').style.visibility='visible';
-                    $('#invalid-password').innerText=error.message;
-                    registerFlag['password'] = false;
-                    break;
-                case 'confirmPassword' :
-                    $('#invalid-confirmPassword').style.visibility='visible';
-                    $('#invalid-confirmPassword').innerText=error.message;
-                    registerFlag['confirmPassword'] = false;
-                    break;
-                case 'name' :
-                    $('#invalid-name').style.visibility='visible';
-                    $('#invalid-name').innerText=error.message;
-                    registerFlag['name'] = false;
-                    break;
-                case 'phoneNumber' :
-                    $('#invalid-phone').style.visibility='visible';
-                    $('#invalid-phone').innerText=error.message;
-                    registerFlag['phone'] = false;
-                    break;
-                case 'address' :
-                    $('#invalid-address').style.visibility='visible';
-                    $('#invalid-address').innerText=error.message;
-                    registerFlag['address'] = false;
-                    break;
-            }
+            $(`strong[name=invalid-${error.field}`).style.visibility='visible';
+            $(`strong[name=invalid-${error.field}`).innerText=error.message;
+            registerFlag[`${error.field}`] = false;
         });
     })
 }
 
 
 function validateCheck(){
-    $('#button').disabled = monitorRegisterButton() ? false : true ;
+    $('#button').disabled = !monitorRegisterButton();
 }
 
 function monitorRegisterButton(){
@@ -108,13 +87,13 @@ function monitorRegisterButton(){
 function validateEmail(){
 
     const regex_email = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\w+\.)+\w+$/i;// 이메일이 적합한지 검사할 정규식
-    const email = $('#email').value;
+    const email = $('input[name=email]').value;
 
     if(!email.match(regex_email)){
-        $('#invalid-email').style.visibility='visible';
+        $('strong[name=invalid-email]').style.visibility='visible';
         registerFlag['email'] = false;
     }else{
-        $('#invalid-email').style.visibility='hidden';
+        $('strong[name=invalid-email]').style.visibility='hidden';
         registerFlag['email'] = true;
         validateCheck();
     }
@@ -122,12 +101,12 @@ function validateEmail(){
 
 function validateName(){
     const regex_name = /[가-힣]{2,16}|[a-zA-Z]{2,16}/;
-    const name = $('#name').value;
+    const name = $('input[name=name]').value;
     if(!name.match(regex_name)){
-        $('#invalid-name').style.visibility='visible';
+        $('strong[name=invalid-name]').style.visibility='visible';
         registerFlag['name'] = false;
     }else{
-        $('#invalid-name').style.visibility='hidden';
+        $('strong[name=invalid-name]').style.visibility='hidden';
         registerFlag['name'] = true;
         validateCheck();
     }
@@ -136,12 +115,12 @@ function validateName(){
 
 function validatePassword(){
     const regex_password = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/;
-    const password = $('#password').value;
+    const password = $('input[name=password]').value;
     if(!password.match(regex_password)){
-        $('#invalid-password').style.visibility='visible';
+        $('strong[name=invalid-password]').style.visibility='visible';
         registerFlag['password'] = false;
     }else{
-        $('#invalid-password').style.visibility='hidden';
+        $('strong[name=invalid-password]').style.visibility='hidden';
         registerFlag['password'] = true;
         validateCheck();
     }
@@ -149,13 +128,13 @@ function validatePassword(){
 }
 
 function validateConfirmPassword(){
-    const confirm = $('#confirm').value;
-    const password = $('#password').value;
+    const confirm = $('input[name=confirm]').value;
+    const password = $('input[name=password]').value;
     if(password !== confirm){
-        $('#invalid-confirmPassword').style.visibility='visible';
+        $('strong[name=invalid-confirmPassword]').style.visibility='visible';
         registerFlag['confirmPassword'] = false;
     }else{
-        $('#invalid-confirmPassword').style.visibility='hidden';
+        $('strong[name=invalid-confirmPassword]').style.visibility='hidden';
         registerFlag['confirmPassword'] = true;
         validateCheck();
     }
@@ -163,52 +142,51 @@ function validateConfirmPassword(){
 }
 function validatePhone(){
     const regex_phone = /(01[016789])-(\d{3,4})-(\d{4})$/;
-    const phone = $('#phone').value;
+    const phone = $('input[name=phoneNumber]').value;
     if(!phone.match(regex_phone)){
-        $('#invalid-phone').style.visibility='visible';
-        registerFlag['phone'] = false;
+        $('strong[name=invalid-phoneNumber]').style.visibility='visible';
+        registerFlag['phoneNumber'] = false;
     }else{
-        $('#invalid-phone').style.visibility='hidden';
-        registerFlag['phone'] = true;
+        $('strong[name=invalid-phoneNumber]').style.visibility='hidden';
+        registerFlag['phoneNumber'] = true;
         validateCheck();
     }
 }
 
 function validateAddress(){
-    const address = $('#address').value;
-    const postcode = $('#postcode').value;
-    if(address && postcode){
-        $('#invalid-address').style.visibility='hidden';
+    const address = $('input[name=address]').value;
+    if(address){
+        $('strong[name=invalid-address]').style.visibility='hidden';
         validateCheck();
         return true;
     }else{
-        $('#invalid-address').style.visibility='visible';
-        $('#invalid-address').innerText='주소를 입력하세요';
+        $('strong[name=invalid-address]').style.visibility='visible';
+        $('strong[name=invalid-address]').innerText='주소를 입력하세요';
         return false;
     }
 }
 
 function validateAddressDetail(){
-    const addressDetail = $('#address-detail').value;
+    const addressDetail = $('input[name=address-detail]').value;
     if(!addressDetail){
-        $('#invalid-address').style.visibility='visible';
-        $('#invalid-address').innerText='상세주소를 입력해주세요';
+        $('strong[name=invalid-address]').style.visibility='visible';
+        $('strong[name=invalid-address]').innerText='상세주소를 입력해주세요';
         registerFlag['addressDetail'] = false;
     }else{
-        $('#invalid-address').style.visibility='hidden';
+        $('strong[name=invalid-address]').style.visibility='hidden';
         registerFlag['addressDetail'] = true;
         validateCheck();
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    $('#signupForm').addEventListener('submit',signupHandler);
-    $('#find-postcode').addEventListener('click',execDaumPostcode);
-    $('#btnCloseLayer').addEventListener('click',closeDaumPostcode);
-    $('#email').onchange = validateEmail;
-    $('#name').onchange = validateName;
-    $('#password').onchange = validatePassword;
-    $('#confirm').onchange = validateConfirmPassword;
-    $('#phone').onchange = validatePhone;
-    $('#address-detail').onchange = validateAddressDetail;
+$('#signupForm').addEventListener('submit',signupHandler);
+$('#btnCloseLayer').addEventListener('click', () => {
+    $('#layer').style.display = 'none';
 });
+
+$('input[name=email]').onchange = validateEmail;
+$('input[name=name]').onchange = validateName;
+$('input[name=password]').onchange = validatePassword;
+$('input[name=confirm]').onchange = validateConfirmPassword;
+$('input[name=phoneNumber]').onchange = validatePhone;
+$('input[name=address-detail]').onchange = validateAddressDetail;
