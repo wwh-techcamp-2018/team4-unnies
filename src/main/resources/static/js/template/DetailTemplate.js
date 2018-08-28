1fafffbb7066a466b71c5302d5b5eb112aa0d7df
Tomadoing <sylim0315@gmail.com>
2018-08-28 17:27:47 +0900

    refactor : reviewTemplate 변수 검증 추가

--- src/main/resources/static/js/template/DetailTemplate.js
+++ src/main/resources/static/js/template/DetailTemplate.js
@@ -1,21 +1,32 @@
+import {translateDateTime} from '../lib/StatusTranslator.js';
+
 export function reviewTemplate({writer, product, rating}) {
+    const writerId = writer.id;
+    const writerName = writer.name;
+    const writerImageUrl = writer.imageUrl ? writer.imageUrl : "/images/blank-profile.png";
+    const writerAddress = writer.address;
+
+    const productName = product.name;
+    const productShareDateTime = translateDateTime(product.shareDateTime);
+    const productRating = rating;
+
     return `<li class='product-comment'>
                <div class='product-comment-wrap'>
                    <div class='product-comment-profile'>
-                    <!--<img alt='${writer.name}-profile' src='${writer.profileImage}'>-->
+                   <img alt='${writerName}-profile' src='${writerImageUrl}' style="width:60px;height:60px;border:dotted 1px lightgray;border-radius:3px;">
                    </div>
                    <div class='product-comment-nickname'>
-                       <a href='#'>${writer.name}</a>
+                       <a href='${writerId}'>${writerName}</a>
                    </div>
     
-                   <div class='product-comment-region-name'>${writer.address}</div>
+                   <div class='product-comment-region-name'>${writerAddress}</div>
                    <div class='product-comment-text'></div>
                    <div>
                      <div class='product-comment-address'>
-                       ${product.name} (${product.shareDateTime}) 후기
+                       ${productName} (${productShareDateTime}) 후기
                     </div>
                     <div id='product-comment-rating'>
-                        ${ratingTemplate(Number(rating))}
+                        ${ratingTemplate(Number(productRating))}
                     </div>
                    </div>
                </div>
