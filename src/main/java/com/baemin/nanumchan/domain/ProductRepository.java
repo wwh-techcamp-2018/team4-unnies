eef9960ebf7a572c23aba823c9f643a9c53b63a6
JiwooHong <tech_jiwoo@woowahan.com>
2018-08-29 04:18:07 +0900

    fix : domain 변경에 따른 native query 수정

--- src/main/java/com/baemin/nanumchan/domain/ProductRepository.java
+++ src/main/java/com/baemin/nanumchan/domain/ProductRepository.java
@@ -13,23 +13,18 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findAllByOwnerIdOrderByIdDesc(Long ownerId, Pageable pageable);
 
-    @Query(value = "SELECT p.id, p.created_at, p.updated_at, p.description, p.title, p.price, p.max_participant, p.expire_date_time, p.is_bowl_needed, p.name, p.share_date_time, p.category_id, p.location_id, p.owner_id " +
-            "FROM (SELECT * " +
-                    ", (SELECT ST_Distance_Sphere(POINT(:longitude, :latitude), POINT(location.longitude, location.latitude)) " +
-                        "FROM location " +
-                        "WHERE location.id = product.location_id) AS distance_meter " +
-                    "FROM product " +
-                    "WHERE NOW() < product.expire_date_time) AS p " +
+    // JPA와는 맞지 않는 구조, 장기적인 측면에서는 JDBC Template 권장
+    @Query(value = "SELECT p.id, p.created_at, p.updated_at, p.title, p.owner_id, p.max_participant, p.expire_date_time, p.price, p.longitude, p.latitude, p.owner_id, p.category_id, p.name, p.description, p.address, p.address_detail, p.share_date_time, p.is_bowl_needed " +
+            "FROM (SELECT *, ST_Distance_Sphere(POINT(:longitude, :latitude), POINT(product.longitude, product.latitude)) AS distance_meter " +
+                "FROM product " +
+                "WHERE NOW() < product.expire_date_time) AS p " +
             "WHERE p.distance_meter < :radiusMeter " +
             "ORDER BY p.distance_meter ASC limit :offset, :limit",
             nativeQuery = true)
     List<Product> findNearProducts(@Param("radiusMeter") int radiusMeter, @Param("longitude") Double longitude, @Param("latitude") Double latitude, @Param("offset") int offset, @Param("limit") int limit);
 
-    @Query(value = "SELECT p.id, p.created_at, p.updated_at, p.description, p.title, p.price, p.max_participant, p.expire_date_time, p.is_bowl_needed, p.name, p.share_date_time, p.category_id, p.location_id, p.owner_id " +
-            "FROM (SELECT * " +
-            ", (SELECT ST_Distance_Sphere(POINT(:longitude, :latitude), POINT(location.longitude, location.latitude)) " +
-                "FROM location " +
-                "WHERE location.id = product.location_id) AS distance_meter " +
+    @Query(value = "SELECT p.id, p.created_at, p.updated_at, p.title, p.owner_id, p.max_participant, p.expire_date_time, p.price, p.longitude, p.latitude, p.owner_id, p.category_id, p.name, p.description, p.address, p.address_detail, p.share_date_time, p.is_bowl_needed " +
+            "FROM (SELECT *, ST_Distance_Sphere(POINT(:longitude, :latitude), POINT(product.longitude, product.latitude)) AS distance_meter " +
                 "FROM product " +
                 "WHERE NOW() < product.expire_date_time) AS p " +
             "WHERE p.distance_meter < :radiusMeter " +
