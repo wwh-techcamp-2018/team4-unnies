757cd9fe0d0313cd8a1aa45ad64b40f6c81a88db
1500sheep <cjswotl6274@naver.com>
2018-08-28 18:07:31 +0900

    chore : orderRepository countByProduct 반환 타입 변경 및 findAllByProduct 추가

--- src/main/java/com/baemin/nanumchan/domain/OrderRepository.java
+++ src/main/java/com/baemin/nanumchan/domain/OrderRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface OrderRepository extends JpaRepository<Order,Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
     int countByProductId(Long productId);
 
     Optional<Order> findByParticipantIdAndProductId(Long participantId, Long productId);
@@ -16,4 +17,6 @@ public interface OrderRepository extends JpaRepository<Order,Long> {
     Page<Order> findAllByParticipantIdOrderByIdDesc(Long participantId, Pageable pageable);
 
     boolean existsByParticipantAndProduct(User user, Product product);
+
+    List<Order> findAllByProduct(Product product);
 }
