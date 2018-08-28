42b78421e8c0d744f5ccfae4617c9cfbcb4de17a
Tomadoing <sylim0315@gmail.com>
2018-08-28 17:27:13 +0900

    feature : cardTemplate naming 수정

--- src/main/resources/static/js/template/CardTemplate.js
+++ src/main/resources/static/js/template/CardTemplate.js
@@ -1,49 +1,53 @@
+import {translateStatus, translateDateTime} from '../lib/StatusTranslator.js';
+import {ratingTemplate} from "../template/DetailTemplate.js";
+
 export function cardTemplate({product, orderCount, status, ownerRating}) {
-
-    const imageUrl = product.productImages.length && product.productImages[0];
-    const name = product.name;
+    //TODO : 클래스 처리
+    const ownerId = product.owner.id;
     const ownerName = product.owner.name;
-    const productStatus = status;//추가
-    const productOrderCount = orderCount ? orderCount : 0 ;//추가
-    const productOwnerRatting = ownerRating;
-    const maxParticipant = product.maxParticipant;
-    const expireDateTime = product.expireDateTime;
-    const price = product.price;
+    const ownerImageUrl = product.owner.imageUrl ? product.owner.imageUrl : "/images/blank-profile.png";
 
-    return `<div class="card" id="mypage-card">
-        <img class="card-img-top" src="https://cdn.bmf.kr/_data/product/I21A3/b3b7ce9d8cf1ee91166fe97785d51d8a.jpg" alt="${name}">
+    const productId = product.id;
+    const productName = product.name;
+    const productImageUrl = product.productImages.length && product.productImages[0];
+    const productStatus = translateStatus(status);
+    const productOrderCount = orderCount ? orderCount : 0;
+    const productOwnerRating = ownerRating;
+    const productMaxParticipant = product.maxParticipant;
+    const productExpireDateTime = translateDateTime(product.expireDateTime);
+    const productPrice = product.price;
+
+    return `
+        <div class="card" id="mypage-card">
+        <a href="../products/${productId}">
+        <img class="card-img-top" src="${productImageUrl}" alt="${productName}">
         <div class="card-body">
-            <h5 class="card-title font-weight-bold">${name}</h5>
+            <h5 class="card-title font-weight-bold">${productName}</h5>
             <!-- chef -->
             <div class="container-fluid mt-2">
                 <div class="row">
-                    <div class="chef-img" style="width:80px;height:80px;border:dotted 1px lightgray;border-radius:3px;"></div>
+                    <img class="chef-img" src="${ownerImageUrl}" style="width:80px;height:80px;border:dotted 1px lightgray;border-radius:3px;">
                     <div class="col text-right">
-                        <p class="card-text">${ownerName}</p>
+                        <a href="${ownerId}">${ownerName}</a>
                         <span class="badge badge-danger">${productStatus}</span>
                     </div>
                 </div>
                 <dl class="rating-app text-right">
                     <dt></dt>
                     <dd class="rating">
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star"></span>
+                       ${ratingTemplate(Number(productOwnerRating))}
                     </dd>
                 </dl>
             </div>
             <dl class="row">
                 <dt class="col">모집인원</dt>
-                <dd class="col">${productOrderCount} / ${maxParticipant}</dd>
+                <dd class="col">${productOrderCount} / ${productMaxParticipant}</dd>
             </dl>
             <dl class="row">
                 <dt class="col">모집기간</dt>
-                <dd class="col">${expireDateTime} <span class="text-muted">까지</span></dd>
+                <dd class="col">${productExpireDateTime} <span class="text-muted">까지</span></dd>
             </dl>
-            <h4 class="card-subtitle text-right font-weight-bold">${price} <span class="text-muted">원</span></h4>
+            <h4 class="card-subtitle text-right font-weight-bold">${productPrice} <span class="text-muted">원</span></h4>
         </div>
-        <!--<a href="/detail.html" class="btn btn-primary">나눔신청</a>-->
     </div>`;
 }
