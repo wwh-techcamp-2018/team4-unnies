8f0effc937821c974ecf65eea617f3a4bddc81ee
1500sheep <cjswotl6274@naver.com>
2018-08-29 09:33:02 +0900

    refactor : product test 추가

--- src/test/java/com/baemin/nanumchan/domain/ProductTest.java
+++ src/test/java/com/baemin/nanumchan/domain/ProductTest.java
@@ -1,5 +1,6 @@
 package com.baemin.nanumchan.domain;
 
+import com.baemin.nanumchan.exception.UnAuthenticationException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ProductTest {
 
     private Product product;
+    private User user;
     private LocalDateTime now;
 
     @Before
@@ -74,5 +76,54 @@ public class ProductTest {
                 .maxParticipant(5)
                 .build();
         assertThat(product.calculateStatus(4)).isEqualTo(Status.ON_PARTICIPATING);
+    }
+
+    @Test
+    public void validateOrder_성공() {
+        user = User.builder()
+                .build();
+        product = Product.builder()
+                .owner(user)
+                .expireDateTime(now.plusDays(1))
+                .maxParticipant(4)
+                .build();
+        product.validateOrder(User.GUEST_USER, false, 5);
+
+    }
+
+    @Test(expected = UnAuthenticationException.class)
+    public void validateOrder_실패_본인() {
+        user = User.builder()
+                .build();
+        product = Product.builder()
+                .owner(user)
+                .expireDateTime(now.plusDays(1))
+                .maxParticipant(4)
+                .build();
+        product.validateOrder(user, false, 3);
+    }
+
+    @Test(expected = UnAuthenticationException.class)
+    public void validateOrder_실패_주문존재() {
+        user = User.builder()
+                .build();
+        product = Product.builder()
+                .owner(user)
+                .expireDateTime(now.plusDays(1))
+                .maxParticipant(4)
+                .build();
+        product.validateOrder(User.GUEST_USER, true, 3);
+    }
+
+    @Test(expected = UnAuthenticationException.class)
+    public void validateOrder_실패_모집꽉참() {
+        user = User.builder()
+                .build();
+        product = Product.builder()
+                .owner(user)
+                .expireDateTime(now.plusDays(1))
+                .maxParticipant(4)
+                .build();
+        product.validateOrder(User.GUEST_USER, true, 4);
     }
 }
