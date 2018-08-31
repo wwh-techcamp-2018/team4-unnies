67f7bedb2be24a6ecbdddda0af55627c0b4e4bd3
sheep1500 <cjswotl6274@naver.com>
2018-08-31 09:15:10 +0900

    refactor : truncate 적용, name 추가 (#161)

--- src/main/resources/static/js/template/CardTemplate.js
+++ src/main/resources/static/js/template/CardTemplate.js
@@ -25,7 +25,7 @@ export function cardTemplate({product, orderCount, status, ownerRating}) {
             <img src="${productImageUrl}" alt="${productName}">
         </div>
         <div class="card-body">
-            <h5 class="card-title font-weight-bold">${productName}</h5>
+            <h5 class="card-title font-weight-bold truncate">${productName}</h5>
             <div class="container-fluid mt-2 chef">
                 <div class="row">
                     <div class="chef-img-container">
@@ -54,45 +54,6 @@ export function cardTemplate({product, orderCount, status, ownerRating}) {
             <h4 class="card-subtitle text-right font-weight-bold price">${numberWithCommas(productPrice)} <span class="text-muted">원</span></h4>
         </div>
     </div>`;
-
-    //
-    // return `
-    //     <div class="card" id="mypage-card">
-    //     <a href="../products/${productId}">
-    //     <div class="card-header">
-    //         <img src="${productImageUrl}" alt="${productName}">
-    //     </div>
-    //     <div class="card-body">
-    //         <h5 class="card-title font-weight-bold">${productName}</h5>
-    //         <!-- chef -->
-    //         <div class="container-fluid mt-2">
-    //             <div class="row">
-    //                 <img class="chef-img" src="${ownerImageUrl}" style="width:80px;height:80px;border:dotted 1px lightgray;border-radius:3px;">
-    //                 <div class="col text-right">
-    //                     <a href="${ownerId}">${ownerName}</a>
-    //                     <span class="badge badge-danger">${productStatus}</span>
-    //                 </div>
-    //             </div>
-    //             <dl class="rating-app text-right">
-    //                 <dt></dt>
-    //                 <dd class="rating">
-    //                    ${ratingTemplate(Number(productOwnerRating))}
-    //                 </dd>
-    //             </dl>
-    //         </div>
-    //         <dl class="row">
-    //             <dt class="col">모집인원</dt>
-    //             <dd class="col">${productOrderCount} / ${productMaxParticipant}</dd>
-    //         </dl>
-    //         <dl class="row">
-    //             <dt class="col">모집기간</dt>
-    //             <dd class="col">${productExpireDateTime} <span class="text-muted">까지</span></dd>
-    //         </dl>
-    //         <h4 class="card-subtitle text-right font-weight-bold">${productPrice} <span class="text-muted">원</span></h4>
-    //     </div>
-    // </div>`;
-
-
 }
 
 
